12edfc0facff726f6dee7f5a6b3628aac0991188
samuelbangtson <samuelbangtson@gmail.com>
2024-03-15 13:19:47 -0400

    Updated Sprite.java
    Built class, more importantly added method for checking collision with other sprites

---
+++ src/application/Sprite.java
@@ -0,0 +1,62 @@
+package application;
+
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+
+public abstract class Sprite extends ImageView{
+
+	private String ID;
+	
+	//Constructors
+	Sprite(){
+		setX(0);
+		setY(0);
+		this.ID = null;
+	}
+	
+	Sprite(String ID, Image png){
+		setImage(png);
+		setX(0);
+		setY(0);
+		this.ID = ID;
+	}
+	Sprite(int x, int y, String ID, Image png){
+		setImage(png);
+		setX(x);
+		setY(y);
+		this.ID = ID;
+	}
+	
+//	getters
+	String getID() {
+		return ID;
+	}
+	
+//	Setters
+	void changeX(int x) {
+		setX(getX() + x);
+	}
+	void changeY(int y) {
+		setY(getY() + y);
+	}
+	
+//	Other methods
+	double getHeight() {
+		double i = getViewport().getHeight();
+		return i;
+	}
+	double getWidth() {
+		double i = getViewport().getWidth();
+		return i;
+	}
+	
+	boolean hasCollisionWith(Sprite target) {
+		if(this.getBoundsInParent().intersects(target.getBoundsInParent())){
+			return true;
+		}
+		else {
+			return false;
+		}
+	}
+
+}
